f0b751009c39c5da8a5a4475f7e6e81bc9de78fa
snimmagadda1 <saiguy@me.com>
2020-11-22 16:40:43 -0600

    Update for bit... not sure why doesn't render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import "./App.css";
 import backgroundImg from "./background.jpg";
 import DiscordForm from "./DiscordForm";
-import { ThemeProvider } from "styled-components";
-
-const theme = {
-  colors: {
-    muted: "#B9BBBE",
-    blue: "#7289da",
-  },
-};
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
         <header className="App-header">
           <DiscordForm />
         </header>
-      </ThemeProvider>
     </div>
   );
 }
--- src/DiscordForm.js
+++ src/DiscordForm.js
@@ -5,12 +5,20 @@ import qr from "./qr.png";
 import GamerForm from "./styled/GamerForm";
 import GamerInput from "./styled/GamerInput";
 import QRStyles from "./styled/QRStyles";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    muted: "#B9BBBE",
+    blue: "#7289da",
+  },
+};
 
 const Container = styled.div`
   display: flex;
   background-color: #363a3e;
   border-radius: 8px;
-  box-shadow: 0 2px 10px 0 rgba(0,0,0,.2);
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
 
   @font-face {
     font-family: catamaran;
@@ -41,39 +49,42 @@ const Container = styled.div`
 class DiscordForm extends React.Component {
   render() {
     return (
-      <Container>
-        <GamerForm>
-          <fieldset>
-            <h3>Welcome back!</h3>
-            <h4>We're so excited to see you again!</h4>
-            <GamerInput>
-              <label htmlFor="ephone">EMAIL OR PHONE NUMBER</label>
-              <input type="text" name="ephone" id="ephone"></input>
-            </GamerInput>
-            <GamerInput>
-              <label htmlFor="password">PASSWORD</label>
-              <input type="password" name="password" id="password"></input>
-              <a>Forgot your password?</a>
-            </GamerInput>
-            <button type="submit">
-              <div>Login</div>
-            </button>
-          </fieldset>
-        </GamerForm>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GamerForm>
+            <fieldset>
+              <h3>Welcome back!</h3>
+              <h4>We're so excited to see you again!</h4>
+              <GamerInput>
+                <label htmlFor="ephone">EMAIL OR PHONE NUMBER</label>
+                <input type="text" name="ephone" id="ephone"></input>
+              </GamerInput>
+              <GamerInput>
+                <label htmlFor="password">PASSWORD</label>
+                <input type="password" name="password" id="password"></input>
+                <a>Forgot your password?</a>
+              </GamerInput>
+              <button type="submit">
+                <div>Login</div>
+              </button>
+            </fieldset>
+          </GamerForm>
 
-        <div class="vert-sep"></div>
+          <div class="vert-sep"></div>
 
-        <QRStyles>
-          <div className="qrMask">
-            <img src={qr}></img>
-          </div>
-          <h3>Log in with QR Code</h3>
-          <h4>
-            Scan this with the <span className="boldDescription">Discord mobile app</span> to log in
-            instantly.
-          </h4>
-        </QRStyles>
-      </Container>
+          <QRStyles>
+            <div className="qrMask">
+              <img src={qr}></img>
+            </div>
+            <h3>Log in with QR Code</h3>
+            <h4>
+              Scan this with the{" "}
+              <span className="boldDescription">Discord mobile app</span> to log
+              in instantly.
+            </h4>
+          </QRStyles>
+        </Container>
+      </ThemeProvider>
     );
   }
 }
