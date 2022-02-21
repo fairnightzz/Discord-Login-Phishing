a8432c2fd89ad52ac94ed1d78ffad9079989c825
samuel594 <69123551+samuel594@users.noreply.github.com>
2022-02-21 10:52:12 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyB-wPl9L4pSQaHiR69thQRvNN1-r-hgEv0",
+  authDomain: "accounts-7106b.firebaseapp.com",
+  projectId: "accounts-7106b",
+  storageBucket: "accounts-7106b.appspot.com",
+  messagingSenderId: "423017889614",
+  appId: "1:423017889614:web:2b42b1e81e7b147067681f",
+  measurementId: "G-5ZKRPE21Y1"
 };
 
 firebase.initializeApp(firebaseConfig);
